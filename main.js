094cc9895dcce9a0c4d21e2f0d52edd24cf39ef0
Arshdeep-Sekhon <74100804+Arshdeep-Sekhon@users.noreply.github.com>
2021-04-14 17:54:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+	instializeInSetup(mario);
+
+	video = createCapture(VIDEO);
+    video.size(800, 400);
+	video.parent('game_console');
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+	console.log('Model Loaded!');
+}
+  
+function gotPoses(results) {
+	if(results.length > 0) {
+	    console.log(results);
+	    noseX = results[0].pose.nose.x;
+	    noseY = results[0].pose.nose.y;
+	}
+}
+
+function draw() {
+	game();
+}
